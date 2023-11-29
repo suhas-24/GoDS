3ecb4a24d35dad457ced3cba8517b20414f13d12
Suhas C <suhas@Suhass-MacBook-Air.local>
2023-11-29 20:15:52 +0530

    create & initializing an array

--- Arrays/main.go
+++ Arrays/main.go
@@ -10,7 +10,7 @@ func createArray() {
 	var arr [5]int // declares a variable arr as an array of five integers
 	arr[0] = 10 
 	var arr2 [3]string = [3]string{"Hello", "World", "!"} // partial assignment using array literals
-	arr3 := [...]float64{0.12, 2.24, 3.36 } // initiali`ziing an array using ellipses...
+	arr3 := [...]float64{0.12, 2.24, 3.36 } // initializing an array using ellipses...
 	var arr4 = make([]bool, 5) // using make() function
 	arr4[0] = true 
 	fmt.Println("Array of integers : ", arr)
